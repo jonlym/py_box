82d83b478f6de356a31b0447093d41eab2c73bc8
Jon Lym <jonathanalym@gmail.com>
2017-04-06 15:32:26 -0400

    Included find_last_modecar in dimer.py

--- py_box/ase/__init__.py
+++ py_box/ase/__init__.py
@@ -4,3 +4,18 @@ Created on Mon Sep 26 16:38:29 2016
 
 @author: Jonathan Lym
 """
+
+from platform import system
+from pprint import pprint
+from ase.visualize import view
+
+def run_testRun(atom_obj):
+    print 'Test Run'
+    os_name = system()
+    if type(atom_obj) is list:        
+        for i in range(len(atom_obj)):    
+            pprint(vars(atom_obj[i]))
+    else:
+        pprint(vars(atom_obj))
+    if os_name.lower() == 'linux':
+        view(atom_obj)
