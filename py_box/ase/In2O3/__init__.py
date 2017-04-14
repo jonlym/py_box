c47ce9a41a40830634d1c2b55744fd2fab84bde2
jonlym <jonathanalym@gmail.com>
2017-04-14 09:51:09 -0400

    Added function to find the closest H atoms

--- py_box/ase/In2O3/__init__.py
+++ py_box/ase/In2O3/__init__.py
@@ -5,6 +5,7 @@ Created on Wed Apr 12 17:12:09 2017
 @author: Jonathan Lym
 """
 
+import heapq
 
 index_dict = {'In1': [26, 27],
               'In2': [24, 25],
@@ -16,3 +17,17 @@ index_dict = {'In1': [26, 27],
               'O4': [73, 72],
               'O5': [43, 42],
               'O6': [68, 69]}
+
+def find_closest_H(atoms, i, n = 1):
+    """Finds the 'n' indices of the H atoms closest to atom 'i'."""
+    all_H_indices = []
+    all_H_dist = []
+    for j, atom in enumerate(atoms):
+        if atom.symbol == 'H':
+            all_H_indices.append(j)
+            all_H_dist.append(atoms.get_distance(i, j))
+    H_low_dist = heapq.nsmallest(n, all_H_dist)
+    H_low_indices = []
+    for dist in H_low_dist:
+        H_low_indices.append(all_H_indices[all_H_dist.index(dist)])
+    return H_low_indices
