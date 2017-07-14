3e7c3e4a7163c719e5b9d3ab355c88a48a69d4ee
Jon Lym <jonathanalym@gmail.com>
2017-07-14 11:03:36 -0400

    Added parity plot option and clean_output options

--- py_box/__init__.py
+++ py_box/__init__.py
@@ -5,6 +5,9 @@ Created on Wed Nov 23 21:10:42 2016
 @author: Jon Lym
 """
 
+import numpy as np
+from matplotlib import pyplot as plt
+
 def any_alpha(string):
     """Returns True if any alphabetic characters are in the string. False otherwise"""
     for character in string:
@@ -22,3 +25,14 @@ def get_unique_list(data):
         keys[item] = 1
     return keys.keys()
 
+def plot_parity(x, y, decimals = 2):
+    data = np.concatenate((x, y), axis = 0)
+    min_data = np.round(min(data*10.**decimals))/10.**decimals
+    max_data = np.round(max(data*10.**decimals))/10.**decimals
+    fig = plt.figure()
+    plt.plot(x, y, 'bo', np.array([min_data, max_data]), np.array([min_data, max_data]), 'k-')
+    axes = plt.gca()
+    axes.set_ylim([min_data, max_data])
+    axes.set_xlim([min_data, max_data])
+#    plt.plot(np.array([min_data, min_data]), np.array([max_data, max_data]), 'k-')
+    return (fig, axes)
