8ea5970dbc06ab39bc071ea74310949504580723
Jon Lym <jonathanalym@gmail.com>
2017-08-22 13:25:27 -0400

    Added fcc sites for Pt surface

--- py_box/ase/Pt/CE/__init__.py
+++ py_box/ase/Pt/CE/__init__.py
@@ -1,1 +1,20 @@
-site_dict = []
+import numpy as np
+
+z_O = 16.872+1.5
+fcc_sites = {0:  np.array([1.403, 0.810, z_O]),
+             1:  np.array([2.805, 3.239, z_O]),
+             2:  np.array([4.208, 5.669, z_O]),
+             3:  np.array([5.611, 8.098, z_O]),
+             4:  np.array([4.208, 0.810, z_O]),
+             5:  np.array([5.611, 3.329, z_O]),
+             6:  np.array([7.013, 5.669, z_O]),
+             7:  np.array([8.416, 8.098, z_O]),
+             8:  np.array([7.013, 0.810, z_O]),
+             9:  np.array([8.416, 3.239, z_O]),
+             10: np.array([9.819, 5.669, z_O]),
+             11: np.array([11.221, 8.098, z_O]),
+             12: np.array([9.819, 0.810, z_O]),
+             13: np.array([11.221, 3.239, z_O]),
+             14: np.array([12.624, 5.669, z_O]),
+             15: np.array([14.027, 8.098, z_O]),
+             }
